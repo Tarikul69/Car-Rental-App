6f25d77da23b1dccf6f9065024e9d163dcef9b2a
tarikulabir <tarikulabir931@gmail.com>
2025-08-03 23:50:55 +0600

    Update

---
+++ lib/widgets/bottomnavigation.dart/bottomnavigation.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class bottomnavigation extends StatefulWidget {
+  const bottomnavigation({super.key});
+
+  @override
+  State<bottomnavigation> createState() => _bottomnavigationState();
+}
+
+class _bottomnavigationState extends State<bottomnavigation> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
